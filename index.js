ae9b0c46a838f9542e9bdedf178e397dc7cde068
Kanny <kanny.ghafour87@gmail.com>
2023-07-11 20:10:46 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(arr, str){
+    let strlowercase = str.toLowerCase();
+
+        const newArr = arr.filter(elemnt => 
+        elemnt.toLowerCase() === strlowercase)
+        return newArr
+
+}
+
+function fuzzyMatch(arr, str) {
+    return arr.filter(element =>
+      element.toLowerCase().startsWith(str.toLowerCase())
+    );
+  }
+  
+
+  function matchName(arr, str){
+    return arr.filter(element =>element.name=== str)
+
+  }
